a06e287a133513ee133feded506c7b95b9ceed86
CaraErskine <caraerskine@gmail.com>
2022-05-31 16:25:20 -0400

    commit tree 7ce5c9de983cba6c58f6719b05800afe16a6d6e4

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+        firstName: "Max",
+        lastName: "Johnson",
+        streetAddress: "4 Lewis Ct.",
+        city: "New Brunswick",
+        state: "NJ",
+        zipCode: "08816",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+     const newObj= {...obj};
+     newObj[key] = value;
+
+     return newObj;
+
+    }    
+    
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    console.log(obj, key, value)   
+    obj[key] = value;
+    console.log(obj)
+    return obj;
+   
+   }
+
+   function deleteFromEmployeeByKey (obj, key){
+       console.log(obj, key)
+       const newObject = {...obj};
+       delete newObject [key]
+       return newObject;
+    }
+
+    function destructivelyDeleteFromEmployeeByKey (obj, key){
+        console.log(obj, key)
+        delete obj[key]
+        return obj;
+    }
